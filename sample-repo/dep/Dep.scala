2f92ec2c0c54b748e3bb4f649b009804f1633c52
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2021-03-01 18:10:03 +0100

    Diagnostics test

--- sample-repo/dep/Dep.scala
+++ sample-repo/dep/Dep.scala
@@ -3,5 +3,5 @@ package dep
 import dep.deeper.DeeperTest
 
 object Dep {
-  val list = List(Test.test() + JavaTest.value + DeeperTest.value)
+  val list = List(Test.test() + JavaTest.value + DeeperTest.value, DeeperTest.value)
 }
